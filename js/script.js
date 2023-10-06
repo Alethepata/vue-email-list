92de96c6f09e421e770f80971272f38c84f3294a
Alethepata <alessia.patane@gmail.com>
2023-10-06 15:20:37 +0200

    [Add] mounted .then( )

--- js/script.js
+++ js/script.js
@@ -4,10 +4,17 @@ createApp({
     data(){
         return{
             url:'https://flynn.boolean.careers/exercises/api/random/mail',
-            email:[]
+            emails:[]
         }
     },
     mounted(){
+        
         axios.get(this.url)
+        .then((risposta)=>{
+            const email = risposta.data.response;
+            this.emails.push(email)
+            console.log(email)
+        })
+        
     }
-}).mount('#id')
+}).mount('#app')
